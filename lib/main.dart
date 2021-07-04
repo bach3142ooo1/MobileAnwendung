99ad4d37b1fb4ce36a8a8252d001489a5fe4f24e
Xuan Bach Le <45541138+bach3142ooo1@users.noreply.github.com>
2021-07-04 14:00:47 +0200

    Calendar hinzufügen mit OnTap-Event

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_anwendungen_flutter/VeranstaltungAnlegen.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.teal,
       ),
       home: SportonaHomePage(title: 'Sportana Home Page'),
@@ -29,7 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class SportonaHomePage extends StatefulWidget {
-  SportonaHomePage({Key? key, required this.title}) : super(key: key);
+  SportonaHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -38,40 +30,21 @@ class SportonaHomePage extends StatefulWidget {
 }
 
 class _SportonaHomePageState extends State<SportonaHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-            children: [
-              OutlinedButton(
-                child: Text('Veranstaltung anlegen'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) {
-                      return VeranstaltungAnlegen();
-                    }),
-                  );
-                },
-              ),
-            ]
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: SfCalendar(
+          view: CalendarView.month,
+          onTap: (calendarTapDetails) {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return VeranstaltungAnlegen();
+            }));
+          },
+        ));
   }
 }
